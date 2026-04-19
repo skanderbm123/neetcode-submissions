09fd14e64805547458a202746553c266ad89cca6
Skander Bm <skander96@hotmail.com>
2026-04-19 14:50:03 -0400

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length - 1; i++) {
+            for (int j = i + 1; j < temperatures.length; j++) {
+                if (temperatures[i] < temperatures[j]) {
+                    result[i] = j - i;
+                    break;
+                } 
+            }
+        }
+        return result;
+    }
+}
