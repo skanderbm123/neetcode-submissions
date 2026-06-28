e712d4b9882b461c3928882d05cc442cd443f137
Skander Bm <skander96@hotmail.com>
2026-06-28 19:11:50 -0400

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for(int i = 0; i < prices.length-1; i++){
+            for (int j=i; j < prices.length; j++){
+                if ((prices[j] - prices[i]) > profit) {
+                    profit = (prices[j] - prices[i]);
+                }
+            }
+        }
+
+            return profit;   
+    }
+}
